5fce6e0cefec5da2050c13a5cd72813d69bbba83
danillotc <danillotc@hotmail.com>
2022-09-12 22:08:24 -0300

    restart button

--- script.js
+++ script.js
@@ -24,11 +24,20 @@ function marcarPosicao(posicao) {
     let alvo = posicoes[posicao.charAt(0)][posicao.charAt(1)];
     if (!alvo) {
         posicoes[posicao.charAt(0)][posicao.charAt(1)] = true;
-        document.getElementById(posicao).innerHTML = jogador1 ? 'X' : 'O';
+        document.getElementById(posicao).innerText = jogador1 ? 'X' : 'O';
         jogador1 = !jogador1;
-        mensagem.innerHTML = `Jogador ${jogador1 ? 1 : 2}, sua vez!`;
+        mensagem.innerText = `Jogador ${jogador1 ? 1 : 2}, sua vez!`;
     } else {
-        mensagem.innerHTML = `Jogador ${jogador1 ? 1 : 2}, esse espaço já foi marcado!`;
+        mensagem.innerText = `Jogador ${jogador1 ? 1 : 2}, esse espaço já foi marcado!`;
     }
-    console.log(posicoes);
+}
+
+function reiniciar() {
+    mensagem.innerText = `Jogo reiniciado! Jogador 1, sua vez!`;
+    for (linha in posicoes) {
+        for (coluna of [1, 2, 3]) {
+            posicoes[linha][coluna] = false;
+            document.getElementById(`${linha+coluna}`).innerText = '';
+        }
+    };
 }
